2042db97cfad402fb4a119e75914ec9347615dde
lokesh739 <lokeshmaheswari2003@gmail.com>
2025-09-01 19:45:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (54.28%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,58 +1,3 @@
-/*class Solution {
-private:
-bool isSafe(int row,int col,vector<string> &board,int n){
-    int duprow=row;
-    int dupcol=col;
-
-    while(row>=0 && col>=0){
-        if(board[row][col]=='Q') return false;
-        row--;
-        col--;
-    }
-
-    row=duprow;
-    col=dupcol;
-    while(col>=0){
-        if(board[row][col]=='Q') return false;
-        col--;
-    }
-
-    row=duprow;
-    col=dupcol;
-    while(row<n && col>=0){
-        if(board[row][col]=='Q') return false;
-        row++;
-        col--;
-    }
-    return true;
-}
-void solve(int col,vector<vector<string>> &ans,vector<string> &board,int n){
-    if(col==n){
-        ans.push_back(board);
-        return;
-    }
-    for(int i=0;i<n;i++){
-        if(isSafe(i,col,board,n)){
-            board[i][col]='Q';
-            solve(col+1,ans,board,n);
-            board[i][col]='.';
-        }
-    }
-}
-public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n);
-        string s(n,'.');
-        for(int i=0;i<n;i++){
-            board[i]=s;
-        }
-        solve(0,ans,board,n);
-        return ans;
-    }
-};
-*/
-
 class Solution {
 private:
 void solve(int col,vector<vector<string>> &ans,vector<string> &board,int n,vector<int> &leftrow,vector<int> &lowerdiagonal,vector<int> &upperdiagonal){
@@ -61,7 +6,7 @@ void solve(int col,vector<vector<string>> &ans,vector<string> &board,int n,vecto
         return;
     }
     for(int row=0;row<n;row++){
-        if(leftrow[row]==0 && lowerdiagonal[row+col]==0 && upperdiagonal[(n-1)+(col-row)]==0){
+        if(leftrow[row]==0 && lowerdiagonal[row+col]==0 && upperdiagonal[n-1+col-row]==0){
             board[row][col]='Q';
             leftrow[row]=1;
             lowerdiagonal[row+col]=1;
@@ -87,3 +32,4 @@ public:
         return ans;
     }
 };
+
